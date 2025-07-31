3e2c5e2785053dfa784f3bbe93e0a3b3e843789e
Christophe Savard <christophe_savard@hotmail.ca>
2025-07-30 20:25:37 -0400

    Add basic setup

---
+++ ImageStitcherCommand.cs
@@ -0,0 +1,18 @@
+﻿using DotMake.CommandLine;
+using Microsoft.Extensions.Logging;
+
+namespace ImageStitcher;
+
+[CliCommand(Description = "Image stitcher utility")]
+public class ImageStitcherCommand(ILogger<ImageStitcherCommand> logger) : ICliRunAsyncWithContextAndReturn
+{
+    private ILogger Logger { get; } = logger;
+
+    /// <inheritdoc />
+    public Task<int> RunAsync(CliContext cliContext)
+    {
+        this.Logger.LogInformation("Hello, world.");
+        cliContext.ShowHelp();
+        return Task.FromResult(0);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DotMake.CommandLine;
+using ImageStitcher;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+Cli.Ext.ConfigureServices(services =>
+{
+    services.AddLogging(builder =>
+    {
+        builder.AddSimpleConsole(options =>
+        {
+            options.SingleLine      = true;
+            options.IncludeScopes   = true;
+            options.UseUtcTimestamp = false;
+            options.TimestampFormat = "HH:mm:ss ";
+        });
+    });
+});
+
+await Cli.RunAsync<ImageStitcherCommand>(args);
